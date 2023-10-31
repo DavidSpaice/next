d49519b1fa750e111b5c68d252397135e1761fa3
Yinan Lu <yinanlu@spaices-MBP-2.ht.home>
2023-10-31 13:13:29 -0400

    updated  Gree Prestige Partnership page

--- app/gree-customer-care/Component.tsx
+++ app/gree-customer-care/Component.tsx
@@ -106,11 +106,10 @@ function CustomerCare() {
         }
     };
 
-
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         setLoading(true);
-        await fetch(`https://airtek-warranty.onrender.com`, {
+        await fetch(`https://airtek-warranty.onrender.com/customer-care/register`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -166,122 +165,127 @@ function CustomerCare() {
                     <div className="loader"></div>
                 </div>
             ) : (
-                <div>
-                    <p className="title">Contact a local dealer</p>
-
-                    <Box
-                        component="div"
-                        sx={{
-                            "& .MuiTextField-root": { m: 1, width: "25ch" },
-                        }}
-                    >
+                <div className='container'>
+                    <div className='form-content'>
                         <div>
-                            <Controls
-                                error={errors.firstName}
-                                type="text"
-                                label="First Name"
-                                name="firstName"
-                                size="small"
-                                value={formData.firstName}
-                                onChange={handleChange}
-                                required
-                            />
-
-                            <Controls
-                                error={errors.lastName}
-                                type="text"
-                                label="Last Name"
-                                name="lastName"
-                                size="small"
-                                value={formData.lastName}
-                                onChange={handleChange}
-                                required
-                            />
+                            <h1 className="title">
+                                Gree Prestige Partnership Dealers!
+                            </h1>
                         </div>
-                        <div>
-                            <Controls
-                                error={errors.email}
-                                required
-                                type="text"
-                                name="email"
-                                label="Email"
-                                size="small"
-                                value={formData.email}
-                                onChange={handleChange}
-                            />
-                            <Controls
-                                error={errors.streetAddress}
-                                type="text"
-                                name="streetAddress"
-                                label="Street Address"
-                                size="small"
-                                value={formData.streetAddress}
-                                onChange={handleChange}
-                                required
-                            />
+                        <br />
+                        <div className='text-center sm:text-left'>
+                            <p>
+                                For the Gree Prestige Partnership Program, we choose to partner with dealers who share our commitment to providing the best customer experience and offer special program benefits designed to accelerate Gree&apos;s leading dealers&apos; growth and overall success.
+                            </p>
                         </div>
-                        <div>
-                            <Controls
-                                error={errors.city}
-                                type="text"
-                                name="city"
-                                label="City"
-                                size="small"
-                                value={formData.city}
-                                onChange={handleChange}
-                                required
-                            />
+                        <br />
+                        <p className="title">Contact a local dealer</p>
 
-                            <Controls
-                                error={errors.stateProvince}
-                                type="text"
-                                name="stateProvince"
-                                label="State / Province"
-                                size="small"
-                                value={formData.stateProvince}
-                                onChange={handleChange}
-                                required
-                            />
+                        <div className='w-full flex flex-col justify-center items-center'>
+                            <Box
+                                component="div"
+                                sx={{
+                                    "& .MuiTextField-root": { m: 1, width: "25ch" },
+                                }}
+                            >
+                                <div>
+                                    <Controls
+                                        error={errors.firstName}
+                                        type="text"
+                                        label="First Name"
+                                        name="firstName"
+                                        size="small"
+                                        value={formData.firstName}
+                                        onChange={handleChange}
+                                        required
+                                    />
+
+                                    <Controls
+                                        error={errors.lastName}
+                                        type="text"
+                                        label="Last Name"
+                                        name="lastName"
+                                        size="small"
+                                        value={formData.lastName}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <Controls
+                                        error={errors.email}
+                                        required
+                                        type="text"
+                                        name="email"
+                                        label="Email"
+                                        size="small"
+                                        value={formData.email}
+                                        onChange={handleChange}
+                                    />
+                                    <Controls
+                                        error={errors.streetAddress}
+                                        type="text"
+                                        name="streetAddress"
+                                        label="Street Address"
+                                        size="small"
+                                        value={formData.streetAddress}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <Controls
+                                        error={errors.city}
+                                        type="text"
+                                        name="city"
+                                        label="City"
+                                        size="small"
+                                        value={formData.city}
+                                        onChange={handleChange}
+                                        required
+                                    />
+
+                                    <Controls
+                                        error={errors.stateProvince}
+                                        type="text"
+                                        name="stateProvince"
+                                        label="State / Province"
+                                        size="small"
+                                        value={formData.stateProvince}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                                <div>
+                                    <Controls
+                                        error={errors.postalCode}
+                                        type="text"
+                                        name="postalCode"
+                                        label="Postal / Zip code"
+                                        size="small"
+                                        value={formData.postalCode}
+                                        onChange={handleChange}
+                                        required
+                                    />
+
+                                    <Controls
+                                        error={errors.phone}
+                                        type="text"
+                                        name="phone"
+                                        label="Phone"
+                                        size="small"
+                                        value={formData.phone}
+                                        onChange={handleChange}
+                                        required
+                                    />
+                                </div>
+                            </Box>
                         </div>
-                        <div>
-                            <Controls
-                                error={errors.postalCode}
-                                type="text"
-                                name="postalCode"
-                                label="Postal / Zip code"
-                                size="small"
-                                value={formData.postalCode}
-                                onChange={handleChange}
-                                required
-                            />
-
-                            {/* <Controls
-                                error={errors.country}
-                                type="text"
-                                name="country"
-                                label="Country"
-                                size="small"
-                                value={formData.country}
-                                onChange={handleChange}
-                                required
-                            /> */}
-
-                            <Controls
-                                error={errors.phone}
-                                type="text"
-                                name="phone"
-                                label="Phone"
-                                size="small"
-                                value={formData.phone}
-                                onChange={handleChange}
-                                required
-                            />
+                        <div className="form-btn">
+                            <button type="submit" className="next-btn" disabled={isDisabled}>
+                                Submit
+                            </button>
                         </div>
-                    </Box>
-                    <div className="form-btn">
-                        <button type="submit" className="next-btn" disabled={isDisabled}>
-                            Submit
-                        </button>
                     </div>
                 </div>
             )}
