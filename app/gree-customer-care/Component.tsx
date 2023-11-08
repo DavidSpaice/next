16fdbc127f971b6bf0558d14970fe291787ce9ab
Yinan Lu <yinanlu@spaices-MBP-2.ht.home>
2023-11-08 14:38:43 -0500

    updated gree customer service

--- app/gree-customer-care/Component.tsx
+++ app/gree-customer-care/Component.tsx
@@ -173,119 +173,168 @@ function CustomerCare() {
                             </h1>
                         </div>
                         <br />
-                        <div className='text-center sm:text-left'>
-                            <p>
-                                For the Gree Prestige Partnership Program, we choose to partner with dealers who share our commitment to providing the best customer experience and offer special program benefits designed to accelerate Gree&apos;s leading dealers&apos; growth and overall success.
-                            </p>
+                        <div className=''>
+                            <div className='w-full'>
+                                <Box className='float-right rounded-lg shadow-2xl ml-4 mb-4' width={350} component="img" src="/customer_service.png" alt="Customer Care" />
+                                <div>
+                                    <h1 className="title">Parts Concession Program</h1>
+                                    <br />
+                                    <h2 className="title">1. Parts Concession</h2>
+                                    <p>One of the standout features of choosing Gree Prestige Partnership is our Parts Concession program, powered by Airtek. This program empowers our valued dealers to concession a Major Component Part within specific time periods, which we refer to as the &quot;Concession Period.&quot; Under this program, Airtek takes care of the Part, while our dealers are responsible for conceding 100% of the labor required for replacement.</p>
+                                    <br />
+                                    <h2 className="title">1.1. Eligible Major Components</h2>
+                                    <p>Our Concession program applies exclusively to the following major components: Compressor, Outdoor coil, Indoor coil, and Heat Exchanger - collectively known as &quot;Major Component Parts&quot; or simply &quot;Parts.&quot;</p>
+                                    <br />
+                                    <h2 className="title">1.2. Concession Periods</h2>
+                                    <p>The Concession Period is determined by the length of the registered limited warranty period. To give you an idea of how this works:</p>
+                                    <ul className='pl-4'>
+                                        <li className="title">For a 10-year registered limited warranty period, the Concession Period extends to 12 months beyond the warranty&apos;s expiration.</li>
+
+                                        <li className="title">With a 5-year registered limited warranty, the Concession Period is 18 months post-expiration.</li>
+
+                                        <li className="title">For a 1-year registered limited warranty, the Concession Period spans 24 months after the warranty&apos;s expiration.</li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
+
                         <br />
                         <p className="title">Contact a local dealer</p>
 
-                        <div className='w-full flex flex-col justify-center items-center'>
-                            <Box
-                                component="div"
-                                sx={{
-                                    "& .MuiTextField-root": { m: 1, width: "25ch" },
-                                }}
-                            >
-                                <div>
-                                    <Controls
-                                        error={errors.firstName}
-                                        type="text"
-                                        label="First Name"
-                                        name="firstName"
-                                        size="small"
-                                        value={formData.firstName}
-                                        onChange={handleChange}
-                                        required
-                                    />
+                        <div className='w-full h-96 flex flex-row justify-center items-center border-2 border-gray-200 rounded-lg shadow-md'>
+                            <div className='w-1/4 h-full flex flex-col pt-12 items-center rounded-s-lg bg-[#182778]'>
+                                <div className='text-white text-center text-xl'>
+                                    <h1>We&apos;re here to help</h1>
+                                </div>
+                                <br />
 
-                                    <Controls
-                                        error={errors.lastName}
-                                        type="text"
-                                        label="Last Name"
-                                        name="lastName"
-                                        size="small"
-                                        value={formData.lastName}
-                                        onChange={handleChange}
-                                        required
-                                    />
+                                <div className='text-white text-center text-xs'>
+                                    <p>
+                                        Contact Customer Care for product, warranty, and dealer inquiries.
+                                    </p>
                                 </div>
-                                <div>
-                                    <Controls
-                                        error={errors.email}
-                                        required
-                                        type="text"
-                                        name="email"
-                                        label="Email"
-                                        size="small"
-                                        value={formData.email}
-                                        onChange={handleChange}
-                                    />
-                                    <Controls
-                                        error={errors.streetAddress}
-                                        type="text"
-                                        name="streetAddress"
-                                        label="Street Address"
-                                        size="small"
-                                        value={formData.streetAddress}
-                                        onChange={handleChange}
-                                        required
-                                    />
+                                <br />
+                                <br />
+                                <div className='text-white text-center text-xs'>
+                                    <i aira-aria-labelledby="phone-icon"><svg fill="none" viewBox="0 0 24 24" className="stroke-[#323232] inline-block w-4 !stroke-white" data-di-rand="1699453406746"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92Z" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                                    </path></svg></i> <a href="tel:+18669691931"> 1-866-969-1931</a>
                                 </div>
-                                <div>
-                                    <Controls
-                                        error={errors.city}
-                                        type="text"
-                                        name="city"
-                                        label="City"
-                                        size="small"
-                                        value={formData.city}
-                                        onChange={handleChange}
-                                        required
-                                    />
+                            </div>
+                            <div className='w-3/4 flex flex-col justify-center items-center'>
+                                <Box
+                                    component="div"
+                                    sx={{
+                                        "& .MuiTextField-root": { m: 1, width: "25ch" },
+                                    }}
+                                >
+                                    <div>
+                                        <Controls
+                                            error={errors.firstName}
+                                            type="text"
+                                            label="First Name"
+                                            name="firstName"
+                                            size="small"
+                                            value={formData.firstName}
+                                            onChange={handleChange}
+                                            required
+                                        />
 
-                                    <Controls
-                                        error={errors.stateProvince}
-                                        type="text"
-                                        name="stateProvince"
-                                        label="State / Province"
-                                        size="small"
-                                        value={formData.stateProvince}
-                                        onChange={handleChange}
-                                        required
-                                    />
-                                </div>
-                                <div>
-                                    <Controls
-                                        error={errors.postalCode}
-                                        type="text"
-                                        name="postalCode"
-                                        label="Postal / Zip code"
-                                        size="small"
-                                        value={formData.postalCode}
-                                        onChange={handleChange}
-                                        required
-                                    />
+                                        <Controls
+                                            error={errors.lastName}
+                                            type="text"
+                                            label="Last Name"
+                                            name="lastName"
+                                            size="small"
+                                            value={formData.lastName}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <Controls
+                                            error={errors.email}
+                                            required
+                                            type="text"
+                                            name="email"
+                                            label="Email"
+                                            size="small"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                        />
+                                        <Controls
+                                            error={errors.streetAddress}
+                                            type="text"
+                                            name="streetAddress"
+                                            label="Street Address"
+                                            size="small"
+                                            value={formData.streetAddress}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <Controls
+                                            error={errors.city}
+                                            type="text"
+                                            name="city"
+                                            label="City"
+                                            size="small"
+                                            value={formData.city}
+                                            onChange={handleChange}
+                                            required
+                                        />
 
-                                    <Controls
-                                        error={errors.phone}
-                                        type="text"
-                                        name="phone"
-                                        label="Phone"
-                                        size="small"
-                                        value={formData.phone}
-                                        onChange={handleChange}
-                                        required
-                                    />
+                                        <Controls
+                                            error={errors.stateProvince}
+                                            type="text"
+                                            name="stateProvince"
+                                            label="State / Province"
+                                            size="small"
+                                            value={formData.stateProvince}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+                                    <div>
+                                        <Controls
+                                            error={errors.postalCode}
+                                            type="text"
+                                            name="postalCode"
+                                            label="Postal / Zip code"
+                                            size="small"
+                                            value={formData.postalCode}
+                                            onChange={handleChange}
+                                            required
+                                        />
+
+                                        <Controls
+                                            error={errors.phone}
+                                            type="text"
+                                            name="phone"
+                                            label="Phone"
+                                            size="small"
+                                            value={formData.phone}
+                                            onChange={handleChange}
+                                            required
+                                        />
+                                    </div>
+
+                                </Box>
+                                <div className="w-full form-btn flex flex-col">
+                                    <div className='w-2/3 text-center text-xs'>
+                                        <p>
+                                            &quot;By clicking &apos;Submit,&apos; I agree to receive information about products and services from a nearby Airtek Ontario Inc. dealer using the provided contact details.
+                                        </p>
+                                    </div>
+                                    <div>
+                                        <button type="submit" className="next-btn" disabled={isDisabled}>
+                                            Submit
+                                        </button>
+                                    </div>
                                 </div>
-                            </Box>
+                            </div>
                         </div>
-                        <div className="form-btn">
-                            <button type="submit" className="next-btn" disabled={isDisabled}>
-                                Submit
-                            </button>
-                        </div>
+
                     </div>
                 </div>
             )}
