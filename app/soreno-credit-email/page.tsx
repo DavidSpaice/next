18252ed2d12c554b66a5cfdc8d4a4e6f2fe49b30
DavidSpaice <support@airtekontario.com>
2024-11-15 14:45:30 -0500

    feat: add soreno credit email

---
+++ app/soreno-credit-email/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import SubmitCreditEmail from './Component'
+
+
+function page() {
+    return (
+        <div><SubmitCreditEmail /></div>
+    )
+}
+
+export default page
