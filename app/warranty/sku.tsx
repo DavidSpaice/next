c0891f41497ff6f698813f84c3bc9f9be59cf65e
Airtek <135278000+DavidSpaice@users.noreply.github.com>
2026-08-05 09:57:51 -0400

    feat: add GWH09ATCXB-D6DNA4D/I model to warranty SKU list

--- app/warranty/sku.tsx
+++ app/warranty/sku.tsx
@@ -359,6 +359,7 @@ const model = [
   { model: "GUD36W2/NHE-D(U)"},
   { model: "GCAC24F/NhE-D(U)"},
   { model: "GWH24AUDXF-D6DNA2J/I"},
+  { model: "GWH09ATCXB-D6DNA4D/I"},
 ];
 
 export default model;
